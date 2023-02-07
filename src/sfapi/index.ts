fd7a136eee554d32b3a571609a99586b65a96e59
Chris Harris <cjh@lbl.gov>
2023-02-07 10:47:16 -0500

    Export generated types

--- src/sfapi/index.ts
+++ src/sfapi/index.ts
@@ -1,1 +1,56 @@
 export { Client } from "./client";
+
+export { ClientBase } from "./_internal";
+export { ApiError } from "./_internal/core/ApiError";
+export { BaseHttpRequest } from "./_internal/core/BaseHttpRequest";
+export {
+  CancelablePromise,
+  CancelError,
+} from "./_internal/core/CancelablePromise";
+export { OpenAPI } from "./_internal/core/OpenAPI";
+export type { OpenAPIConfig } from "./_internal/core/OpenAPI";
+
+export type { ComputeCommandOutput } from "./_internal/models/ComputeCommandOutput";
+export { ComputeStatus } from "./_internal/models/ComputeStatus";
+export type { Status } from "./_internal/models/Status";
+export { StorageStatus } from "./_internal/models/StorageStatus";
+export type { CommandOutput } from "./_internal/models/CommandOutput";
+export { UtilitiesStatus } from "./_internal/models/UtilitiesStatus";
+export { BatchGroupAction } from "./_internal/models/BatchGroupAction";
+export type { CreateGroupBody } from "./_internal/models/CreateGroupBody";
+export type { RunCommandBody } from "./_internal/models/RunCommandBody";
+export type { TransferStorageBody } from "./_internal/models/TransferStorageBody";
+export type { SubmitJobBody } from "./_internal/models/SubmitJobBody";
+export type { UpdateGroupBody } from "./_internal/models/UpdateGroupBody";
+export type { UploadFileBody } from "./_internal/models/UploadFileBody";
+export type { Changelog } from "./_internal/models/Changelog";
+export type { Config } from "./_internal/models/Config";
+export type { DirectoryEntry } from "./_internal/models/DirectoryEntry";
+export type { DirectoryOutput } from "./_internal/models/DirectoryOutput";
+export type { FileDownload } from "./_internal/models/FileDownload";
+export type { GroupList } from "./_internal/models/GroupList";
+export type { GroupStats } from "./_internal/models/GroupStats";
+export type { HTTPValidationError } from "./_internal/models/HTTPValidationError";
+export type { JobOutput } from "./_internal/models/JobOutput";
+export type { Note } from "./_internal/models/Note";
+export type { Outage } from "./_internal/models/Outage";
+export type { ProjectStats } from "./_internal/models/ProjectStats";
+export { PublicHost } from "./_internal/models/PublicHost";
+export type { QueueOutput } from "./_internal/models/QueueOutput";
+export { StatusValue } from "./_internal/models/StatusValue";
+export type { StorageStats } from "./_internal/models/StorageStats";
+export type { Task } from "./_internal/models/Task";
+export type { Tasks } from "./_internal/models/Tasks";
+export type { TransferResult } from "./_internal/models/TransferResult";
+export type { UploadResult } from "./_internal/models/UploadResult";
+export type { UserInfo } from "./_internal/models/UserInfo";
+export type { UserStats } from "./_internal/models/UserStats";
+export type { ValidationError } from "./_internal/models/ValidationError";
+
+export { AccountService } from "./_internal/services/AccountService";
+export { ComputeService } from "./_internal/services/ComputeService";
+export { MetaService } from "./_internal/services/MetaService";
+export { StatusService } from "./_internal/services/StatusService";
+export { StorageService } from "./_internal/services/StorageService";
+export { TasksService } from "./_internal/services/TasksService";
+export { UtilitiesService } from "./_internal/services/UtilitiesService";
